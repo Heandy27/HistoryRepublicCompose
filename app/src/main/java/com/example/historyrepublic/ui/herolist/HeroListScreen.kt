533332904aa99e04a4aee883031ea2349d45300c
Andy <andy_heredia2008@hotmail.com>
2026-01-27 16:30:00 -0500

    HeroListScreen design changed

--- app/src/main/java/com/example/historyrepublic/ui/herolist/HeroListScreen.kt
+++ app/src/main/java/com/example/historyrepublic/ui/herolist/HeroListScreen.kt
@@ -1,10 +1,15 @@
 package com.example.historyrepublic.ui.herolist
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -12,6 +17,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,9 +43,12 @@ fun HeroListScreen_Preview() {
 @Composable
 fun HeroListItem(hero: Hero, modifier: Modifier = Modifier) {
     Card(modifier = modifier.fillMaxWidth()) {
-        Column(modifier = Modifier.fillMaxWidth().padding(8.dp)) {
-            AsyncImage(model = hero.image, contentDescription = "Logo")
-            Text(hero.nameHero, style = MaterialTheme.typography.headlineLarge)
+        Row(modifier = Modifier.fillMaxWidth().padding(8.dp), horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+            AsyncImage(model = hero.image, contentDescription = "Logo", modifier = Modifier.size(100.dp, 150.dp), contentScale = ContentScale.Crop)
+            Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+                Text(hero.nameHero, style = MaterialTheme.typography.headlineLarge)
+                Text(hero.information, style = MaterialTheme.typography.bodySmall)
+            }
         }
     }
 }
